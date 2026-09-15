003bb4fe1c94d855a798d92684af82f70f180db1
madeyuliya03-web <madeyuliya03@gmail.com>
2026-09-15 21:01:03 +0800

    Add database configuration file

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php
+// Konfigurasi Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'olshop_baju');
+
+// Membuat koneksi
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+// Set charset untuk mendukung karakter Indonesia
+$conn->set_charset("utf8");
+?>
